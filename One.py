ea5b4f5894a0d02f52daa559dfeceac3b77e93fd
tosxin-456 <127943389+tosxin-456@users.noreply.github.com>
2023-11-07 22:18:50 +0100

    added if statements

--- One.py
+++ One.py
@@ -2,6 +2,7 @@ gay = 1
 peepe = 3
 c = peepe + gay
 #my very first python comment
+z = 3
 
 x = 5 + 2j
 a = str(input('what is your name '));
@@ -10,3 +11,10 @@ c = int(input('your bank card number'));
 d = bool(input('are you fine? (true or false)'))
 e = a + b + c + d 
 print(e)
+
+if z < 0:
+  input('z is an integer less than zero')
+elif z>0:
+  input('z is an integer greater than zero')
+else:
+  input('z is not a number')
