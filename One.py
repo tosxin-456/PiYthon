c49c13ade04c60f1fd3c95031f1bece6a1d4650e
tosxin-456 <127943389+tosxin-456@users.noreply.github.com>
2023-11-07 22:01:15 +0100

    added input values

--- One.py
+++ One.py
@@ -4,4 +4,9 @@ c = peepe + gay
 #my very first python comment
 
 x = 5 + 2j
-# print()
+a = str(input('what is your name '));
+b = str(input('how old are you'));
+c = int(input('your bank card number'));
+d = bool(input('are you fine? (true or false)'))
+e = a + b + c + d 
+print(e)
