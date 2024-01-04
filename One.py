733fb914a31027912a73e9401cbbefd842d63f62
tosxin-456 <127943389+tosxin-456@users.noreply.github.com>
2024-01-04 19:58:09 +0100

    started a backend server

--- One.py
+++ One.py
@@ -1,20 +1,52 @@
-gay = 1
-peepe = 3
-c = peepe + gay
-#my very first python comment
-z = 3
+student_count = 10000
+# the above students count is a string.
+rating = 9.999
+# this is a decimal point also known as float 
+is_published = False
+# this is called a boolean and is either true or false
+course_name = "python"
+# this is a single type of string it is only on one line and either single or double qoutes can be used
+course_names = """
+hello world
+how are you world?
+what did you do world"""
+# this is the naming of a system where different lines of code can be used with triple quotes either double or triple
 
-x = 5 + 2j
-a = str(input('what is your name '));
-b = str(input('how old are you'));
-c = int(input('your bank card number'));
-d = bool(input('are you fine? (true or false)'))
-e = a + b + c + d 
-print(e)
+x = 1
+y = 1
+# this is a naming on a single line
 
-if z < 0:
-  input('z is an integer less than zero')
-elif z>0:
-  input('z is an integer greater than zero')
-else:
-  input('z is not a number')
+a,b = 1,2
+# this is when naming two variables in one line
+
+c = d = 3
+
+
+# a variable will be converted from one form to another 
+
+age : int  = 20
+age = "python"
+print(id(age))
+
+# id is the location that a value is stored
+
+x = [1,2,3,4,5]
+x.append(6)
+# len(x)
+print(x[2])
+# append adds a number to the last digit of an array
+# len checks for the number of items 
+f = print(x[0:-1])
+# in this case, python passes from the first to the last number that was stated
+print(f)
+# / is an escape character
+# \n is for a new line 
+message = "python\n escape character"
+
+print(message)
+
+first = "tosin"
+last = "poppins"
+
+full = first + " " +  last
+print(full )
