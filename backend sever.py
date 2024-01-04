0e1c4096adf77fbf5f40916b72379219f0032534
tosxin-456 <127943389+tosxin-456@users.noreply.github.com>
2024-01-04 20:01:56 +0100

    started a backend server

--- backend sever.py
+++ backend sever.py
@@ -1,3 +1,9 @@
 from flask import Flask, request
 
-app = Flask(__name__)
+app = Flask(__name__);
+
+@app.route('/register', methods=["Post"])
+
+def newForm():
+    data  = request.form
+    return "New user registered"
