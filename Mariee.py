d323f9c3457b839a816fea5d1c38489905c44e9d
tosxin-456 <127943389+tosxin-456@users.noreply.github.com>
2024-01-26 22:39:12 +0100

    added another line

---
+++ Mariee.py
@@ -0,0 +1,38 @@
+y = "6"
+z = "7"
+
+a = y + z
+print(a)
+
+
+x = 10 #this is for defining no. 10 with a variable.
+m = 15 #this is for the definition of mariee
+
+
+M  = 10 #an integer 
+A = 10.0 #a float
+R = " Maria is a girl \n and is also a woman"
+I  = 'Maria is a woman'
+
+print(R[3:7])
+fruits  = ['banana', 'plaintain', 'apple', 'beans']
+
+
+
+
+
+# for Kris in fruits:
+#  print(Kris)
+
+# x = 0
+# for i in range (9999):
+#  print(x)
+
+# for i in range (7056):
+#  x += 1
+#  print (x)
+
+
+# print(x)
+ 
+
