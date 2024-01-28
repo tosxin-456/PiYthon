fc3cbc67cd9aec8fe232e7f060df215d5112c16d
tosxin-456 <127943389+tosxin-456@users.noreply.github.com>
2024-01-28 13:46:51 +0100

    added a few lines

--- Mariee.py
+++ Mariee.py
@@ -11,14 +11,20 @@ m = 15 #this is for the definition of mariee
 
 M  = 10 #an integer 
 A = 10.0 #a float
-R = " Maria is a girl \n and is also a woman"
+R = "Maria is a girl \n and is also a woman"
 I  = 'Maria is a woman'
 
-print(R[3:7])
 fruits  = ['banana', 'plaintain', 'apple', 'beans']
+print(fruits[0:2])
 
+p = pow(3,4)
+st = 3&4
+print (p)
 
-
+num  = 0x535
+print(num)
+# for i in range (1,10):
+#  print('Hello world')
 
 
 # for Kris in fruits:
