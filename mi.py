94b4e43f7d42c262747edbea1567aa97f2696522
tosxin-456 <127943389+tosxin-456@users.noreply.github.com>
2024-02-06 23:09:57 +0100

    hello

--- mi.py
+++ mi.py
@@ -3,7 +3,7 @@ flot_t  = 1.0
 string_ = "hello", 'hello '
 Bool = True  # that is a boolean 
 List = [1,1.0, "one", True]
-tuple_ = (1,1.0,"one")
+# tuple_ = (1,1.0,"one")
 # dictionery_ = {one:1,two:2}
 # print(len(List))
 # def helloWordl:
