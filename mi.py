21c426c353a3c2a50f1626d6a662c04e73841d52
tosxin-456 <127943389+tosxin-456@users.noreply.github.com>
2024-02-07 21:55:33 +0100

    Line

--- mi.py
+++ mi.py
@@ -2,7 +2,7 @@ intege_r = 1
 flot_t  = 1.0
 string_ = "hello", 'hello '
 Bool = True  # that is a boolean 
-List = [1,1.0, "one", True]
+# List = [1,1.0, "one", True]
 # tuple_ = (1,1.0,"one")
 # dictionery_ = {one:1,two:2}
 # print(len(List))
