d903644089ac0faa76af6f0c3e89f78e856a6ef1
tosxin-456 <127943389+tosxin-456@users.noreply.github.com>
2024-02-08 22:00:39 +0100

    hello

--- mi.py
+++ mi.py
@@ -1,7 +1,7 @@
 intege_r = 1
 flot_t  = 1.0
 string_ = "hello", 'hello '
-Bool = True  # that is a boolean 
+# Bool = True  # that is a boolean 
 # List = [1,1.0, "one", True]
 # tuple_ = (1,1.0,"one")
 # dictionery_ = {one:1,two:2}
